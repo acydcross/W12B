f6acd3de7c45dac6289ac9c0c8d19cd6e23ff7d6
acydcross <acydcross@hotmail.com>
2020-09-13 20:23:27 -0600

    created a PATCH and DELETE that console.log

--- js/app.js
+++ js/app.js
@@ -12,9 +12,9 @@ function sendTweet() {
     ajax.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 201) {
             console.log(JSON.parse(this.responseText));
-            document.getElementById('status').innerHTML = "Success!";
+            document.getElementById('status').innerHTML = "Sent!";
         } else if(this.readyState != 4) {
-            document.getElementById('status').innerHTML = "Loading...";
+            document.getElementById('status').innerHTML = "Sending...";
         } else {
             document.getElementById('status').innerHTML = "Something Went Wrong!";
         }
@@ -27,3 +27,50 @@ function sendTweet() {
 
 let tweetButton = document.getElementById('tweet-submit');
 tweetButton.addEventListener('click', sendTweet);
+
+function patchTweet() {
+    let tweetTitle = document.getElementById('title-input').value;
+    let tweetBody = document.getElementById('body-input').value;
+    
+    let tweetData = {
+        title: tweetTitle,
+        body: tweetBody,
+        userId: 1
+    };
+    
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(this.responseText));
+        } else if(this.readyState != 4) {
+            console.log("Patching...");;
+        } else {
+            console.log("Something Went Wrong!");
+            console.log(this.status);
+        }
+    }
+
+    ajax.open('PATCH', 'https://jsonplaceholder.typicode.com/posts/1', true);
+    ajax.setRequestHeader('Content-Type', 'application/json');
+    ajax.send(JSON.stringify(tweetData));
+}
+
+patchTweet();
+
+function deleteTweet() {
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        } else if(this.readyState != 4) {
+            console.log('deleting...');
+        } else {
+            console.log('delete fails');
+        }
+    }
+
+    ajax.open('DELETE', 'https://jsonplaceholder.typicode.com/posts/1', true);
+    ajax.send();
+}
+
+deleteTweet();
